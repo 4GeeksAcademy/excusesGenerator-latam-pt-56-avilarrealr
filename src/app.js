a571b7a7bb3a06dc232fa6b31097680a469c50a6
Avilarrealr <149917560+Avilarrealr@users.noreply.github.com>
2025-08-16 00:42:19 +0000

    Adding all files whit Excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+
+// Boton de reload page
+  const reload = document.getElementById("reload");
+  reload.addEventListener("click", (_) => {
+    location.reload();
+  });
+  
+  let randomNumber = Math.floor(Math.random() * 10);
+  document.querySelector('#excuse').innerHTML = generateExcuse()
 };
+
+const generateExcuse = () => {
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird']
+  let action = ['ate', 'peed', 'crushed', 'broke']
+  let what = ['my homework', 'my phone', 'the car']
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']
+
+  let whoIndex = Math.floor(Math.random() * who.length)
+  let actionIndex = Math.floor(Math.random() * action.length)
+  let whatIndex = Math.floor(Math.random() * what.length)
+  let whenIndex = Math.floor(Math.random() * when.length)
+
+  return who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex]
+}
